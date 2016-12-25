1ed8fbae032fd1833c8e71aa396f3fe6a6510455
Bello <Kakawu@lanciao.com>
2016-12-25 21:16:13 +0800

    Second

---
+++ gulpfile.js
@@ -0,0 +1,49 @@
+var gulp        = require('gulp');
+var sass        = require('gulp-sass');
+var pug         = require('gulp-pug');
+var postcss     = require('gulp-postcss');
+
+var browserSync = require('browser-sync').create();
+
+var autoprefixer = require('autoprefixer');
+var resType = require('postcss-responsive-type');
+
+gulp.task('serve', ['sass', 'pug'], function() {
+    browserSync.init({
+        server: {
+            baseDir: "./dist",
+            index: 'index.html'
+        }
+    });
+
+    gulp.watch('src/stylesheets/**', ['sass']);
+    gulp.watch('src/pugfiles/**', ['pug']);
+    gulp.watch("dist/*.html").on('change', browserSync.reload);
+});
+
+
+gulp.task('sass', function () {
+    var processors = [
+      autoprefixer,
+      resType
+    ];
+    return gulp.src('src/stylesheets/main.scss')
+        .pipe(sass({
+            onError: browserSync.notify
+        }))
+        .pipe(postcss(processors))
+        .pipe(gulp.dest('dist/assests/css'))
+        .pipe(browserSync.reload({stream:true}));
+});
+
+
+gulp.task('pug', function(){
+  return gulp.src('src/pugfiles/index.pug')
+  .pipe(pug({
+    pretty: true
+  }))
+  .pipe(gulp.dest('dist/'))
+  .pipe(browserSync.reload({stream:true}));
+});
+
+gulp.task('default', ['serve']);
